f28d80adfeda2c41643f6b159e80ee98479021de
EricOliveira90 <ericfaria@gmail.com>
2021-11-03 18:26:18 -0300

    test: add artifact controller should return 400 if missing required params

--- src/presentation/controller/add-artifact-controller.spec.ts
+++ src/presentation/controller/add-artifact-controller.spec.ts
@@ -1,10 +1,13 @@
+import { MissingParamError } from "../errors";
 import { AddArtifactController } from "./add-artifact-controller"
+
+// Should return 400 if missing required params
 
 describe ('Add Artifact Controller', () => {
     test('Should return 400 if no set is provided', () => {
         const sut = new AddArtifactController();
-        const httpRequest = {
-            set: 'any_set',
+        const httpRequest = { body: {
+            /* set: 'any_set', */
             type: 'any_type',
             level: 20,
             mainstat: 'any_stat',
@@ -14,13 +17,98 @@ describe ('Add Artifact Controller', () => {
                 {substat: 'any_stat', value: 0},
                 {substat: 'any_stat', value: 0}
             ]
-        };
+        }};
         const httpResponse = sut.handle(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('set'));
     })
 })
 
-// Same as above for all fields
+describe ('Add Artifact Controller', () => {
+    test('Should return 400 if no type is provided', () => {
+        const sut = new AddArtifactController();
+        const httpRequest = { body: {
+            set: 'any_set',
+            /* type: 'any_type', */
+            level: 20,
+            mainstat: 'any_stat',
+            substats: [
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0}
+            ]
+        }};
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('type'));
+    })
+})
+
+describe ('Add Artifact Controller', () => {
+    test('Should return 400 if no level is provided', () => {
+        const sut = new AddArtifactController();
+        const httpRequest = { body: {
+            set: 'any_set',
+            type: 'any_type',
+            /* level: 20, */
+            mainstat: 'any_stat',
+            substats: [
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0}
+            ]
+        }};
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('level'));
+    })
+})
+
+describe ('Add Artifact Controller', () => {
+    test('Should return 400 if no mainstat is provided', () => {
+        const sut = new AddArtifactController();
+        const httpRequest = { body: {
+            set: 'any_set',
+            type: 'any_type',
+            level: 20,
+            /* mainstat: 'any_stat', */
+            substats: [
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0}
+            ]
+        }};
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('mainstat'));
+    })
+})
+
+describe ('Add Artifact Controller', () => {
+    test('Should return 400 if no substat is provided', () => {
+        const sut = new AddArtifactController();
+        const httpRequest = { body: {
+            set: 'any_set',
+            type: 'any_type',
+            level: 20,
+            mainstat: 'any_stat',
+            /* substats: [
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0},
+                {substat: 'any_stat', value: 0}
+            ] */
+        }};
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('substats'));
+    })
+})
+
+
 
 // Set, type, mainstat and substats should be part of their enums
 
