e9f334e91f9aa2b1786ed92ce4268d4cd6a92eb1
EricOliveira90 <ericfaria@gmail.com>
2021-11-12 22:17:04 -0300

    chore: add express

---
+++ src/main/server.ts
@@ -0,0 +1,10 @@
+import { MongoHelper } from '../infra/artifact/db/mongodb/mongo-helper'
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const { setupApp } = await import('./config/app')
+    const app = await setupApp()
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
