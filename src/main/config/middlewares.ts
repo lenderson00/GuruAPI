11b68b9bc76662bd79c26dc353be0933b04aa6f0
EricOliveira90 <ericfaria@gmail.com>
2021-11-12 23:30:59 -0300

    feat: ensure requests parse body as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,10 @@
 /* import { bodyParser, cors, contentType } from '@/main/middlewares' */
 
 import { Express } from 'express'
+import { bodyParser } from '../middlewares/body-parser'
 
 export default (app: Express): void => {
+  app.use(bodyParser)
+/*   app.use(cors)
+  app.use(contentType) */
 }
