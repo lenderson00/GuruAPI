bcc5978005acb73a8248b23ca81da5e345bf6ecc
EricOliveira90 <ericfaria@gmail.com>
2021-11-19 17:10:09 -0300

    test: ensure GetArtifactDB throw if GetArtifactRepo throws

--- src/data/artifact/usecases/get-artifact.spec.ts
+++ src/data/artifact/usecases/get-artifact.spec.ts
@@ -1,3 +1,4 @@
+import { throwError } from "../../../../tests/mocks/test-helper"
 import { GetArtifactDB } from "./get-artifact"
 import { getArtifactRepoSpy } from "./mock-artifact-db"
 
@@ -12,8 +13,16 @@ describe ('Get-Artifact-DB Usecase', () => {
     test('Should call GetArtifactRepo with correct values', async () => {
         const { sut, getArtifactRepoStub } = makeSut()
         const gelArtifactSpy = jest.spyOn(getArtifactRepoStub, 'get')
-        const id = { id: "valid_id"}
+        const id = { id: "valid_id" }
         await sut.get(id)
         expect(gelArtifactSpy).toHaveBeenCalledWith(id)
+    })
+
+    test('Should throw if GetArtifactRepo throws', async () => {
+        const { sut, getArtifactRepoStub } = makeSut()
+        jest.spyOn(getArtifactRepoStub, 'get').mockImplementationOnce(throwError)
+        const id = { id: "valid_id" }
+        const promise = sut.get(id)
+        await expect(promise).rejects.toThrow()
     })
 })
