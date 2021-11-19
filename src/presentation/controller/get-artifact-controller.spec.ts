34a8bd04184abc149b1ee2ab36c2852f45bb26db
EricOliveira90 <ericfaria@gmail.com>
2021-11-19 12:23:56 -0300

    test: ensure GetArtifactController return 200 if GetArtifact returns data

--- src/presentation/controller/get-artifact-controller.spec.ts
+++ src/presentation/controller/get-artifact-controller.spec.ts
@@ -1,4 +1,6 @@
 import { throwError } from "../../../tests/mocks/test-helper"
+import { upgradeTiers } from "../../data/artifact/chances"
+import { Sets, Stats, Types } from "../../data/artifact/enums"
 import { GetArtifact, GetArtifactResult } from "../../domain/artifact/usecases/crud-artifact"
 import { InvalidParamError, MissingParamError, ServerError } from "../errors"
 import { serverError } from "../helpers/http-helper"
@@ -47,5 +49,30 @@ describe ('Get Artifact Controller', () => {
         const httpRequest: Request = { id: 'invalid_id' }
         const httpResponse = await sut.handle(httpRequest);
         expect(httpResponse).toEqual(serverError(new ServerError()))
+    })
+
+    test('Should return 200 if successful', async () => {
+        const { sut, getArtifactStub } = makeSut();
+        jest.spyOn(getArtifactStub, 'get').mockImplementationOnce(async () => new Promise((res) => res({
+            set: Sets.AP,
+            type: Types.Flower,
+            level: 20,
+            mainstat: Stats.ATKFlat,
+            mainstatValue: 311,
+            substats: [{substat: Stats.CD, value: Math.round(upgradeTiers[Stats.CD][3])}],
+            score: 200
+        })))
+        const httpRequest = { id: 'valid_id' }
+        const httpResponse = await sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(200);
+        expect(httpResponse.body).toEqual({
+            set: Sets.AP,
+            type: Types.Flower,
+            level: 20,
+            mainstat: Stats.ATKFlat,
+            mainstatValue: 311,
+            substats: [{substat: Stats.CD, value: Math.round(upgradeTiers[Stats.CD][3])}],
+            score: 200
+        });
     })
 })
