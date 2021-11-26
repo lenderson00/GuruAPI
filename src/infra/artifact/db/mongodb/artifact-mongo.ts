c041e91ae6463811fa9643029c444e423b319032
EricOliveira90 <ericfaria@gmail.com>
2021-11-26 18:54:21 -0300

    fix: rollback ArtifactMongo crash

--- src/infra/artifact/db/mongodb/artifact-mongo.ts
+++ src/infra/artifact/db/mongodb/artifact-mongo.ts
@@ -1,23 +1,20 @@
-import { Collection, ObjectId } from "mongodb";
+import { ObjectId } from "mongodb";
 import { AddArtifactRepo, AddArtifactRepoParams, AddArtifactRepoResult } from "../../../../data/artifact/protocols/add-artifact-repo"
 import { DelArtifactRepo, DelArtifactRepoParams, DelArtifactRepoResult } from "../../../../data/artifact/protocols/del-artifact-repo";
 import { GetArtifactRepo, GetArtifactRepoParams, GetArtifactRepoResult } from "../../../../data/artifact/protocols/get-artifact-repo";
 import { MongoHelper } from "./mongo-helper";
 
 export class ArtifactMongo implements AddArtifactRepo, DelArtifactRepo, GetArtifactRepo {
-    private artifactCollection: Collection
-
-    constructor () {
-        this.artifactCollection = MongoHelper.getCollection('artifacts')
-    }
-
+    
     async add (artifactData: AddArtifactRepoParams): Promise<AddArtifactRepoResult> {
-        const result = await this.artifactCollection.insertOne(artifactData)
+        const artifactCollection = MongoHelper.getCollection('artifacts')
+        const result = await artifactCollection.insertOne(artifactData)
         return result.insertedId !== null
     }
 
     async del (id: DelArtifactRepoParams): Promise<DelArtifactRepoResult> {
-        const result = await this.artifactCollection.deleteOne( { "_id" : new ObjectId(id) } )
+        const artifactCollection = MongoHelper.getCollection('artifacts')
+        const result = await artifactCollection.deleteOne( { "_id" : new ObjectId(id) } )
         return result.deletedCount === 1
     }
 
