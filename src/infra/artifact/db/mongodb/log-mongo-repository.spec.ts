4053155543ec253a273b3aedd45331e80e33e4ca
EricOliveira90 <ericfaria@gmail.com>
2021-11-29 12:53:22 -0300

    test: add log controller decorator

---
+++ src/infra/artifact/db/mongodb/log-mongo-repository.spec.ts
@@ -0,0 +1,34 @@
+import { LogMongoRepository } from './log-mongo-repository'
+import { MongoHelper } from './mongo-helper'
+
+import { Collection } from 'mongodb'
+import faker from 'faker'
+import env from '../../../../main/config/env'
+
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
+let errorCollection: Collection
+
+describe('LogMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+    await sut.logError(faker.random.words())
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
