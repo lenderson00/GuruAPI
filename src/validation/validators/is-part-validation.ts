9b7d96fe1c63029d0b162e9c80fad18e3cdbedc3
EricOliveira90 <ericfaria@gmail.com>
2021-11-30 18:43:12 -0300

    feat: add isPartValidation for all basic artifact data

--- src/validation/validators/is-part-validation.ts
+++ src/validation/validators/is-part-validation.ts
@@ -1,5 +1,7 @@
 import { Validation } from '../../presentation/protocols'
 import { InvalidParamError } from '../../presentation/errors'
+import { Set, Type, Level, MainStat, SubStat } from '../../data/artifact/utils/enums'
+import { allSets, allTypes, allLevels, allMainStats, allSubStats } from '../../data/artifact/utils/combinations'
 
 export class isPartValidation<T> implements Validation {
   constructor (private readonly fieldName: string, private readonly arrayName: Array<T>) {}
@@ -8,4 +10,20 @@ export class isPartValidation<T> implements Validation {
     if (!this.arrayName.includes(input[this.fieldName])) return new InvalidParamError(this.fieldName)
     return null
   }
+}
+
+export class isArtifactSetValidation extends isPartValidation<Set> {
+  constructor () { super('set', allSets) }
+}
+export class isArtifactTypeValidation extends isPartValidation<Type> {
+  constructor () { super('type', allTypes) }
+}
+export class isArtifactLevelValidation extends isPartValidation<Level> {
+  constructor () { super('level', allLevels) }
+}
+export class isArtifactMainStatValidation extends isPartValidation<MainStat> {
+  constructor () { super('mainstat', allMainStats) }
+}
+export class isArtifactSubStatValidation extends isPartValidation<SubStat> {
+  constructor () { super('substat', allSubStats) }
 }
