358b97f522d8a572882c96bf6987a7a0f3180f2d
EricOliveira90 <ericfaria@gmail.com>
2021-11-30 21:44:29 -0300

    test: ensure isAllowedMainStat Validation returns null if validation succeeds

---
+++ src/validation/validators/is-allowed-validation.spec.ts
@@ -0,0 +1,26 @@
+import { InvalidParamError } from '../../presentation/errors'
+import { Sets, Stats, Types } from '../../data/artifact/utils/enums'
+import { isAllowedMainStatValidation } from './is-allowed-validation'
+
+describe('isAllowedValidation', () => {
+  
+  describe('isAllowedMainStatValidation', () => {
+    /* test('Should return a InvalidParamError if validation fails', () => {
+      const sut = new isAllowedMainStatValidation()
+      const error = sut.validate({
+          type: Types.Flower,
+          mainstat: Stats.Anemo
+      })
+      expect(error).toEqual(new InvalidParamError('mainstat'))
+    }) */
+
+    test('Should not return if validation succeeds', () => {
+      const sut = new isAllowedMainStatValidation()
+      const error = sut.validate({
+        type: Types.Flower,
+        mainstat: Stats.ATKFlat
+    })
+      expect(error).toBeFalsy()
+    })
+  })
+})
