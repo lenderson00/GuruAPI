61ad1f815c9971f7e43b544b867d4cd7f1a4b7da
EricOliveira90 <ericfaria@gmail.com>
2021-11-30 22:26:03 -0300

    feat: add isAllowedSubStat Validation by mainstat

--- src/validation/validators/is-allowed-validation.ts
+++ src/validation/validators/is-allowed-validation.ts
@@ -1,5 +1,5 @@
-import { allowedMainStats } from "../../data/artifact/utils/combinations";
-import { Type } from "../../data/artifact/utils/enums";
+import { allowedMainStats, allowedSubStats } from "../../data/artifact/utils/combinations";
+import { MainStat, Stats, SubStat, SubStats, Type } from "../../data/artifact/utils/enums";
 import { InvalidParamError } from "../../presentation/errors";
 import { Validation } from "../../presentation/protocols";
 
@@ -7,5 +7,17 @@ export class isAllowedMainStatValidation implements Validation {
     validate (input: any): Error | null {
         if (!allowedMainStats[input.type as Type].includes(input.mainstat as never)) return new InvalidParamError('mainstat')
         return null
+      }
+}
+
+export class isAllowedSubStatValidation implements Validation {
+    validate (input: any): Error | null {
+        let result = null
+        input.substats.forEach((substat: { substat: SubStat; }) => {
+            if (!allowedSubStats[input.mainstat as MainStat].includes(substat.substat)) {
+                result =  new InvalidParamError('substat')
+            }
+        });
+        return result
       }
 }
