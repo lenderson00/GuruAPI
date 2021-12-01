ec77216425b1c5143c6546720a4f64e45df761bc
EricOliveira90 <ericfaria@gmail.com>
2021-11-30 21:33:37 -0300

    feat: move addArtifactController tests to Validation composite

--- src/main/factories/controller/add-artifact-validation-controller.ts
+++ src/main/factories/controller/add-artifact-validation-controller.ts
@@ -1,10 +1,16 @@
 import { ValidationComposite, RequiredFieldValidation } from '../../../validation/validators'
 import { Validation } from '../../../presentation/protocols'
+import { isArtifactLevelValidation, isArtifactSetValidation, isArtifactTypeValidation } from '../../../validation/validators/is-part-validation'
 
 export const makeSignUpValidation = (): ValidationComposite => {
+  
   const validations: Validation[] = []
   for (const field of ['set', 'type', 'level', 'mainstat', 'substats']) {
     validations.push(new RequiredFieldValidation(field))
   }
+  validations.push(new isArtifactSetValidation())
+  validations.push(new isArtifactTypeValidation())
+  validations.push(new isArtifactLevelValidation())
+
   return new ValidationComposite(validations)
 }
