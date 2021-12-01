2c68263aa02949c8067db704b119bd2f323cc4d4
EricOliveira90 <ericfaria@gmail.com>
2021-11-30 21:55:36 -0300

    feat: ensure isAllowedMainStat Validation returns an InvalidParamError if validation fails

--- src/validation/validators/is-allowed-validation.ts
+++ src/validation/validators/is-allowed-validation.ts
@@ -1,7 +1,11 @@
+import { allowedMainStats } from "../../data/artifact/utils/combinations";
+import { Type } from "../../data/artifact/utils/enums";
+import { InvalidParamError } from "../../presentation/errors";
 import { Validation } from "../../presentation/protocols";
 
 export class isAllowedMainStatValidation implements Validation {
     validate (input: any): Error | null {
+        if (!allowedMainStats[input.type as Type].includes(input.mainstat as never)) return new InvalidParamError('mainstat')
         return null
       }
 }
