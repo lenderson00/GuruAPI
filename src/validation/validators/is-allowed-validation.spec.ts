5f96755bd37d79efb55f5af52ba874639fde2c57
EricOliveira90 <ericfaria@gmail.com>
2021-11-30 21:55:43 -0300

    test: ensure isAllowedMainStat Validation returns an InvalidParamError if validation fails

--- src/validation/validators/is-allowed-validation.spec.ts
+++ src/validation/validators/is-allowed-validation.spec.ts
@@ -5,20 +5,20 @@ import { isAllowedMainStatValidation } from './is-allowed-validation'
 describe('isAllowedValidation', () => {
   
   describe('isAllowedMainStatValidation', () => {
-    /* test('Should return a InvalidParamError if validation fails', () => {
+    test('Should return a InvalidParamError if validation fails', () => {
       const sut = new isAllowedMainStatValidation()
       const error = sut.validate({
           type: Types.Flower,
           mainstat: Stats.Anemo
       })
       expect(error).toEqual(new InvalidParamError('mainstat'))
-    }) */
+    })
 
     test('Should not return if validation succeeds', () => {
       const sut = new isAllowedMainStatValidation()
       const error = sut.validate({
         type: Types.Flower,
-        mainstat: Stats.ATKFlat
+        mainstat: Stats.HPFlat
     })
       expect(error).toBeFalsy()
     })
