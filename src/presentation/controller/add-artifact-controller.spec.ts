ae6240ebee1b70fa8dcb5f855dee04f92c1bb014
EricOliveira90 <ericfaria@gmail.com>
2021-12-02 14:26:17 -0300

    test: ensure addArtifactController returns 500 if AddArtifactDB throws and remove unused tests

--- src/presentation/controller/add-artifact-controller.spec.ts
+++ src/presentation/controller/add-artifact-controller.spec.ts
@@ -1,8 +1,10 @@
 import { ValidationSpy } from "../../../tests/mocks/mock-validation";
+import { throwError } from "../../../tests/mocks/test-helper";
 import { upgradeTiers } from "../../data/artifact/utils/chances";
-import { Sets, Stats, Type, Types } from "../../data/artifact/utils/enums";
+import { Sets, Stats, Types } from "../../data/artifact/utils/enums";
 import { AddArtifact, AddArtifactResult } from "../../domain/artifact/usecases/crud-artifact";
-import { InvalidParamError, MissingParamError } from "../errors";
+import { MissingParamError, ServerError } from "../errors";
+import { serverError } from "../helpers";
 import { AddArtifactController, Request } from "./add-artifact-controller"
 
 const makeSut = () => {
@@ -30,46 +32,6 @@ const makeFakeRequest = (): Request => ({
 })
 
 describe ('Add Artifact Controller', () => {
-    
-    // Set, type, mainstat and substats should be part of their combinations
-        
-    test('Should return 400 if any substat value is invalid', async () => {
-        const { sut } = makeSut();
-        const httpRequest = makeFakeRequest()
-        httpRequest.substats![3] = {substat: Stats.DEFFlat, value: 0}; // Def flat substat value of zero cannot exist
-        const httpResponse = await sut.handle(httpRequest);
-        expect(httpResponse.statusCode).toBe(400);
-        expect(httpResponse.body).toEqual(new InvalidParamError('substat value: DEF'));
-    })
-    
-    test('Should return 400 if more than 4 substats are provided', async () => {
-        const { sut } = makeSut();
-        const httpRequest = makeFakeRequest()
-        httpRequest.substats = [
-            {substat: Stats.ATK, value: Math.round(upgradeTiers["ATK%"][0]*10)/10},
-            {substat: Stats.ATKFlat, value: Math.round(upgradeTiers.ATK[0])},
-            {substat: Stats.DEF, value: Math.round(upgradeTiers["DEF%"][0]*10)/10},
-            {substat: Stats.DEFFlat, value: Math.round(upgradeTiers.DEF[0])},
-            {substat: Stats.EM, value: Math.round(upgradeTiers["Elemental Mastery"][0])}, // 5th substat cannot exist
-        ]
-        const httpResponse = await sut.handle(httpRequest);
-        expect(httpResponse.statusCode).toBe(400);
-        expect(httpResponse.body).toEqual(new InvalidParamError('# of substats'));
-    })
-
-    test('Should return 400 if substats values indicate more than 5 upgrade rolls', async () => {
-        const { sut } = makeSut();
-        const httpRequest = makeFakeRequest()
-        httpRequest.substats = [
-            {substat: Stats.ATK, value: Math.round(upgradeTiers["ATK%"][0]*3*10)/10},
-            {substat: Stats.ATKFlat, value: Math.round(upgradeTiers.ATK[0]*3)},
-            {substat: Stats.DEF, value: Math.round(upgradeTiers["DEF%"][0]*3*10)/10},
-            {substat: Stats.DEFFlat, value: Math.round(upgradeTiers.DEF[0]*3)}
-        ]
-        const httpResponse = await sut.handle(httpRequest);
-        expect(httpResponse.statusCode).toBe(400);
-        expect(httpResponse.body).toEqual(new InvalidParamError('Invalid # of rolls: 8'));
-    })
 
     test('Should call AddArtifactDB with correct data', async () => {
         const { sut, addArtifactStub } = makeSut();
@@ -92,6 +54,13 @@ describe ('Add Artifact Controller', () => {
         jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
         const httpResponse = await sut.handle(makeFakeRequest());
         expect(httpResponse.statusCode).toBe(400);
+    })    
+
+    test('Should return 500 if AddArtifact throws', async () => {
+        const { sut, addArtifactStub } = makeSut();
+        jest.spyOn(addArtifactStub, 'add').mockImplementationOnce(throwError)
+        const httpResponse = await sut.handle(makeFakeRequest());
+        expect(httpResponse).toEqual(serverError(new ServerError()))
     })
 
     test('Should return 200 if valid data is provided', async () => {
