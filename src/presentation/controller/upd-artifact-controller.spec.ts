e25008a0e3796fb14f75f23afb863580d47b74f5
EricOliveira90 <ericfaria@gmail.com>
2021-12-02 17:09:49 -0300

    test: ensure updArtifactController calls UpdArtifactDB with correct data

---
+++ src/presentation/controller/upd-artifact-controller.spec.ts
@@ -0,0 +1,41 @@
+import {  } from "fs"
+import { ValidationSpy } from "../../../tests/mocks/mock-validation"
+import { upgradeTiers } from "../../data/artifact/utils/chances"
+import { Sets, Types, Stats } from "../../data/artifact/utils/enums"
+import { UpdArtifact, UpdArtifactResult } from "../../domain/artifact/usecases/crud-artifact"
+import { UpdArtifactController, Request } from "./upd-artifact-controller"
+
+const makeSut = () => {
+    const updArtifactStub: UpdArtifact = {
+        update: async () => {
+            return new Promise((res) => res(true as UpdArtifactResult))
+        }
+    }
+    const validationStub = new ValidationSpy()
+    const sut = new UpdArtifactController(updArtifactStub, validationStub)
+    return { sut, updArtifactStub, validationStub }
+}
+
+const makeFakeRequest = (): Request => ({
+    id: '123456789012345678901234',
+    set: Sets.AP,
+    type: Types.Flower,
+    level: 20,
+    mainstat: Stats.HPFlat,
+    substats: [
+        {substat: Stats.ATK, value: Math.round(upgradeTiers["ATK%"][0]*2*10)/10},
+        {substat: Stats.ATKFlat, value: Math.round(upgradeTiers.ATK[0]*3)},
+        {substat: Stats.DEF, value: Math.round(upgradeTiers["DEF%"][0]*10)/10},
+        {substat: Stats.DEFFlat, value: Math.round(upgradeTiers.DEF[0]*2)},
+    ]
+})
+
+describe ('Add Artifact Controller', () => {
+    test('Should call UpdArtifactDB with correct data', async () => {
+        const { sut, updArtifactStub } = makeSut();
+        const updArtifactSpy = jest.spyOn(updArtifactStub, 'update')
+        const httpRequest = makeFakeRequest();
+        await sut.handle(httpRequest);
+        expect(updArtifactSpy).toHaveBeenCalledWith(httpRequest);
+    })
+})
