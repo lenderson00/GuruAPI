195ffaad7f4ac1311604450ff06266935178833a
EricOliveira90 <ericfaria@gmail.com>
2021-12-02 17:46:59 -0300

    feat: ensure updArtifactController calls GetArtifactDB with correct data

--- src/presentation/controller/upd-artifact-controller.ts
+++ src/presentation/controller/upd-artifact-controller.ts
@@ -1,18 +1,25 @@
-import { UpdArtifact, UpdArtifactParams, UpdArtifactResult } from "../../domain/artifact/usecases/crud-artifact";
-import { ok } from "../helpers";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Character } from "../../data/character/character";
+import { GetArtifact, GetArtifactResults, UpdArtifact, UpdArtifactParams, UpdArtifactResult } from "../../domain/artifact/usecases/crud-artifact";
+import { badRequest, ok } from "../helpers";
 import { Controller, HttpResponse, Validation } from "../protocols";
 
 export class UpdArtifactController implements Controller {
     private readonly updArtifact: UpdArtifact
+    private readonly getArtifact: GetArtifact
     private readonly validation: Validation
 
-    constructor (updArtifact: UpdArtifact, validation: Validation) {
+    constructor (updArtifact: UpdArtifact, getArtifact: GetArtifact, validation: Validation) {
         this.updArtifact = updArtifact
+        this.getArtifact = getArtifact;
         this.validation = validation
     }
 
     async handle (request: Request): Promise<HttpResponse> {
-        const isOk: UpdArtifactResult = await this.updArtifact.update(request as UpdArtifactParams);
+        const error = this.validation.validate(request)
+        if (error) return badRequest(error)
+        const asisArtifact: GetArtifactResults = await this.getArtifact.get({ids: [request.id!]})
+        const isOk: UpdArtifactResult = await this.updArtifact.update(request as UpdArtifactParams)
         return ok(isOk)
     }
 }
@@ -24,4 +31,5 @@ export interface Request {
     level?: number
     mainstat?: string
     substats?: {substat: string, value: number}[]
+    char?: Character
 }
