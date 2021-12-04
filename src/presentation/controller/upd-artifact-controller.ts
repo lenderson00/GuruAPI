89f92d1b35c2dca1a40bf63c5b94fa9f9a39a343
EricOliveira90 <ericfaria@gmail.com>
2021-12-03 22:11:54 -0300

    feat: ensure updArtifactController returns 500 if UpdArtifactDB throws

--- src/presentation/controller/upd-artifact-controller.ts
+++ src/presentation/controller/upd-artifact-controller.ts
@@ -3,7 +3,7 @@ import { Character } from "../../data/character/character";
 import { UpdArtifact, UpdArtifactParams, UpdArtifactResult } from "../../domain/artifact/usecases/crud-artifact";
 import { RequiredFieldValidation } from "../../validation/validators";
 import { isArtifactLevelValidation, isArtifactMainStatValidation, isArtifactSetValidation, isArtifactSubStatValidation, isArtifactTypeValidation } from "../../validation/validators/is-part-validation";
-import { badRequest, ok } from "../helpers";
+import { badRequest, ok, serverError } from "../helpers";
 import { Controller, HttpResponse } from "../protocols";
 
 export class UpdArtifactController implements Controller {
@@ -14,26 +14,30 @@ export class UpdArtifactController implements Controller {
     }
 
     async handle (request: Request): Promise<HttpResponse> {
+        try {
+            let error = (new RequiredFieldValidation('id')).validate(request)
+            if (error) return badRequest(error)
+            if (request.set) error = (new isArtifactSetValidation).validate(request)
+            if (error) return badRequest(error)
+            if (request.type) error = (new isArtifactTypeValidation).validate(request)
+            if (error) return badRequest(error)
+            if (request.level) error = (new isArtifactLevelValidation).validate(request)
+            if (error) return badRequest(error)
+            if (request.mainstat) error = (new isArtifactMainStatValidation).validate(request)
+            if (error) return badRequest(error)
+            if (request.substats)
+                request.substats.forEach(sub => {
+                    error = error || (new isArtifactSubStatValidation).validate(sub)
+                })
+            if (error) return badRequest(error)
+    
+            const isOk: UpdArtifactResult = await this.updArtifact.update(request as UpdArtifactParams)
+            if (isOk instanceof Error) return badRequest(isOk)
+            return ok(isOk)    
+        } catch (error) {
+            return serverError(error as Error)
+        }
         
-        let error = (new RequiredFieldValidation('id')).validate(request)
-        if (error) return badRequest(error)
-        if (request.set) error = (new isArtifactSetValidation).validate(request)
-        if (error) return badRequest(error)
-        if (request.type) error = (new isArtifactTypeValidation).validate(request)
-        if (error) return badRequest(error)
-        if (request.level) error = (new isArtifactLevelValidation).validate(request)
-        if (error) return badRequest(error)
-        if (request.mainstat) error = (new isArtifactMainStatValidation).validate(request)
-        if (error) return badRequest(error)
-        if (request.substats)
-            request.substats.forEach(sub => {
-                error = error || (new isArtifactSubStatValidation).validate(sub)
-            })
-        if (error) return badRequest(error)
-
-        const isOk: UpdArtifactResult = await this.updArtifact.update(request as UpdArtifactParams)
-        if (isOk instanceof Error) return badRequest(isOk)
-        return ok(isOk)
     }
 }
 
