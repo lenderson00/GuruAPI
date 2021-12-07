69e785f401c641b67f499dc587500bfae8c857bb
EricOliveira90 <ericfaria@gmail.com>
2021-12-07 19:27:05 -0300

    feat: ensure UpdArtifactRepo returns false if update fails

--- src/data/artifact/protocols/upd-artifact-repo.ts
+++ src/data/artifact/protocols/upd-artifact-repo.ts
@@ -7,17 +7,17 @@ export interface UpdArtifactRepo {
 
 export type UpdArtifactRepoParams = {
     id: string
-    level: Level
-    mainstatValue: number
-    substats: {substat: SubStat, value: number}[]
-    dtModified: string
-    scoreDflt: number
-    scoreDfltMainstat: number
-    scoreDfltSubstats: number
-    scoreDfltLvl20Min: number
-    scoreDfltLvl20Avg: number
-    scoreDfltLvl20Max: number
-    scoreDfltLvl20SD: number
+    level?: Level
+    mainstatValue?: number
+    substats?: {substat: SubStat, value: number}[]
+    dtModified?: string
+    scoreDflt?: number
+    scoreDfltMainstat?: number
+    scoreDfltSubstats?: number
+    scoreDfltLvl20Min?: number
+    scoreDfltLvl20Avg?: number
+    scoreDfltLvl20Max?: number
+    scoreDfltLvl20SD?: number
     user?: string
     char?: Character
 }
--- src/infra/artifact/db/mongodb/artifact-mongo.ts
+++ src/infra/artifact/db/mongodb/artifact-mongo.ts
@@ -1,11 +1,11 @@
 import { ObjectId } from "mongodb";
-import { UpdArtifactRepoParams, UpdArtifactRepoResult } from "../../../../data/artifact/protocols";
+import { UpdArtifactRepo, UpdArtifactRepoParams, UpdArtifactRepoResult } from "../../../../data/artifact/protocols";
 import { AddArtifactRepo, AddArtifactRepoParams, AddArtifactRepoResult } from "../../../../data/artifact/protocols/add-artifact-repo"
 import { DelArtifactRepo, DelArtifactRepoParams, DelArtifactRepoResult } from "../../../../data/artifact/protocols/del-artifact-repo";
 import { GetArtifactRepo, GetArtifactRepoParams, GetArtifactRepoResults } from "../../../../data/artifact/protocols/get-artifact-repo";
 import { MongoHelper } from "./mongo-helper";
 
-export class ArtifactMongo implements AddArtifactRepo, DelArtifactRepo, GetArtifactRepo {
+export class ArtifactMongo implements AddArtifactRepo, DelArtifactRepo, GetArtifactRepo, UpdArtifactRepo {
     
     async add (artifactData: AddArtifactRepoParams): Promise<AddArtifactRepoResult> {
         const artifactCollection = MongoHelper.getCollection('artifacts')
@@ -16,7 +16,7 @@ export class ArtifactMongo implements AddArtifactRepo, DelArtifactRepo, GetArtif
     async del (id: DelArtifactRepoParams): Promise<DelArtifactRepoResult> {
         const artifactCollection = MongoHelper.getCollection('artifacts')
         const result = await artifactCollection.deleteOne( { "_id" : new ObjectId(id) } )
-        return result.deletedCount === 1
+        return result.deletedCount == 1
     }
 
     async get (artifactData: GetArtifactRepoParams): Promise<GetArtifactRepoResults> {
@@ -32,7 +32,10 @@ export class ArtifactMongo implements AddArtifactRepo, DelArtifactRepo, GetArtif
     }
 
     async update (artifactData: UpdArtifactRepoParams): Promise<UpdArtifactRepoResult> {
-        
-        return true
+        const artifactCollection = MongoHelper.getCollection('artifacts')
+        const { id, ...updateData } = artifactData
+        const result = await artifactCollection.updateOne({ _id: new ObjectId(id) }, { $set: updateData})
+        console.log(result)
+        return (result.matchedCount == 1 && result.modifiedCount == 1)
     }
 }
