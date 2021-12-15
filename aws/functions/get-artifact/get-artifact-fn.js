a82aad5d0d2331de7f2e258aeb7f6fd97ea6fa7e
EricOliveira90 <ericfaria@gmail.com>
2021-12-15 13:32:12 -0300

    refactor: create configuration files for AWS Lambda deployment

---
+++ aws/functions/get-artifact/get-artifact-fn.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var route_adapter = require("main/adapters/express-route-adapter");
+var artifactControllers = require("main/factories/controller/artifact");
+exports.handler = async (event) => {
+    const fn = (0, route_adapter.adaptRoute)((0, artifactControllers.makeGetArtifactController)());
+    const result = fn.handle(event);
+    return result;
+};
--- src/main/functions/addArtifactFn.ts
+++
@@ -1,8 +0,0 @@
-import { HttpResponse } from "../../presentation/protocols";
-import { makeAddArtifactController } from "../factories/controller/artifact";
-
-export const handler = async (event: any, context: any, callback: any): Promise<HttpResponse> => {
-    const func = makeAddArtifactController()
-    const result = func.handle(event)
-    return result
-}
