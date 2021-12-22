608ed33ce623c45582e177412fa67a80e0446658
EricOliveira90 <ericfaria@gmail.com>
2021-12-22 01:19:00 -0300

    test: add log error dynamo repository

---
+++ src/infra/db/dynamodb/log-error/log-dynamo-repository.spec.ts
@@ -0,0 +1,23 @@
+import { LogMongoRepository } from './log-dynamo-repository'
+import faker from 'faker'
+import env from '../../../../main/config/env'
+import { DynamoHelper } from '../dynamo-helper'
+
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+const dynamoHelper = new DynamoHelper()
+const dynamo = dynamoHelper.getLocalDynamo()
+
+describe('Log-Dynamo-Repository', () => {
+  beforeAll(async () => {
+    await dynamoHelper.deleteAllFromTable(env.aws.dynamoLogErrorTableName)
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+    await sut.logError(faker.random.words())
+    const data = await dynamo.scan({ TableName: env.aws.dynamoLogErrorTableName }).promise()
+    expect(data.Count).toBe(1)
+  })
+})
