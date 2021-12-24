87e1ac2942889a70895832efe93a18fff36a417b
EricOliveira90 <ericfaria@gmail.com>
2021-12-24 14:55:23 -0300

    fix: remove mongo connection from lambda functions

--- aws/functions/upd-artifact/upd-artifact-fn.js
+++ aws/functions/upd-artifact/upd-artifact-fn.js
@@ -4,12 +4,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const artifact_1 = require("main/factories/controller/artifact");
 const aws_lambda_route_adapter_1 = require("main/adapters/aws-lambda-route-adapter");
-const MongoHelper_1 = require("infra/artifact/db/mongodb/mongo-helper");
-const env = require("main/config/env");
 
 exports.handler = async (event) => {
-    await MongoHelper_1.MongoHelper.connect(env.mongoUrl)
     const UpdArtifactRouteController = aws_lambda_route_adapter_1.adaptRoute(artifact_1.makeUpdArtifactController());
-    await MongoHelper_1.MongoHelper.disconnect()
     return UpdArtifactRouteController(event);
 };
