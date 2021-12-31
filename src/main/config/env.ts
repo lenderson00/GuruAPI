e879a89b90dbfa7331c7b55bfc8adee6f2bb6e28
EricOliveira90 <ericfaria@gmail.com>
2021-12-31 18:37:17 -0300

    fix: adjustments for aws deployment

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -6,7 +6,7 @@
 } */
 
 export default {
-    mongoURL: process.env.MONGO_URL /* || getMongodbConnString(), 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000' */,
+    /* mongoURL: process.env.MONGO_URL */ /* || getMongodbConnString(), 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000', */
     port: process.env.PORT || 5050,
     aws: {
         dynamoArtifactTableName: 'guru-artifacts-local',
@@ -14,7 +14,7 @@ export default {
         dynamoAPIVersion: '2012-08-10',
         dynamoEndpoint: process.env.DYNAMO_URL || 'http://localhost:2077',
         region: 'us-east-1',
-        accessKeyId: 'AKIAQEQG6ZSACYLQR6MK',
-        secretAccessKey: '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'
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_KEY
     }
 }
